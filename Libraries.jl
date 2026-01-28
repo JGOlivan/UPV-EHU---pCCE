ca705fb3328ef96f4bf28e72f09ceeffef91b052
Jaime García Oliván <jaime.garcia@ehu.eus>
2026-01-28 15:25:47 +0100

    Small fix

--- Libraries.jl
+++ Libraries.jl
@@ -6,6 +6,4 @@ using ArgParse, Distributed, MKL, Revise, ProgressMeter, Dates, BenchmarkTools, 
 using SlurmClusterManager # only for HPC cluster usage
 
 using Plots
-using LsqFit        # only needed for plotting
-using Polynomials   # only needed for plotting
 using LaTeXStrings  # for writting LaTeX stuff (i think only for plotting)
